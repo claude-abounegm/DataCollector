13f6e94f71dae560ea948145e1001ec4d6a908dc
claude-abounegm <claudenegm@gmail.com>
2017-03-31 23:54:16 -0400

    updated sensor.py.. might not work tho

--- sensor.py
+++ sensor.py
@@ -1,46 +1,40 @@
 #!/usr/bin/python
 
-import os;
-import glob;
-import time;
-import json;
-import sys;
-import requests;
-import math;
+import os
+import glob
+import time
+import sys
 
-# FOR LIGHT DATA #
-import RPi.GPIO as GPIO, time, os      
- 
-DEBUG = 1
+import json
+import requests
+
+import math
+
+import RPi.GPIO as GPIO
+
 GPIO.setmode(GPIO.BCM)
-# END OF FOR LIGHT DATA #
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
 
-#ABBY
-DIR = '/home/pi/thermostat';
-PAUSE_INTERVAL = 5;
-TEMP_UPDATE_URL = "http://127.0.0.1:3000/api/temp"; # Replace with your remote URL
-LIGHT_UPDATE_URL = "http://127.0.0.1:3000/api/light"; # Replace with your remote URL
-VERBOSE = True;
+BASE_DIR = '/sys/bus/w1/devices/'
 
-TEMP_SENSOR_ID = "TEST"; # Change this to your public sensor ID.
-TEMP_SENSOR_KEY = "28-0516b2d083ff"; # Change this to your private sensor key.
-LIGHT_SENSOR_ID = "TEST"; # Change this to your public sensor ID.
-LIGHT_SENSOR_KEY = "TEST"; # Change this to your private sensor key.
+DEBUG = 1
+READ_DELAY = 0.5
+AVERAGE_COUNT = 3
 
-MIN_VALUE = 0
-MAX_VALUE = 800000.0 #100000.0
-#END ABBY
+TEMP_SENSOR_ID = "TEMP0"
+TEMP_SENSOR_KEY = "28-0516b2d083ff"
+TEMP_UPDATE_URL = "http://127.0.0.1:3000/api/temp"
 
-os.system('modprobe w1-gpio');
-os.system('modprobe w1-therm');
+LIGHT_SENSOR_ID = "LIGHT0"
+LIGHT_SENSOR_KEY = "LIGHT0"
+LIGHT_UPDATE_URL = "http://127.0.0.1:3000/api/light"
+LIGHT_MIN_VALUE = 0.0
+LIGHT_MAX_VALUE = 800000.0
+LIGHT_PIN = 18
 
-READ_DELAY = 0.5;
-AVERAGE_COUNT = 3;
-
-BASE_DIR = '/sys/bus/w1/devices/';
-
-def get_device_file():
-	global BASE_DIR;
+# START TEMPERATURE #
+def get_temperature_file():
 	file = None;
 	try:
 		file = glob.glob(BASE_DIR + '28*')[0] + '/w1_slave';
@@ -48,50 +42,30 @@ def get_device_file():
 		pass;
 	return file;
 
-def read_temp_raw(device_file):
-	lines = None;
-	with open(device_file, 'r') as file:
-		lines = file.readlines();
-	return lines;
+def read_temperature(file):
+	lines = None
+	equals_pos = -1
 
-def read_temp(device_file):
-	lines = None;
-	equals_pos = -1;
 	while equals_pos < 0 or lines[0].strip()[-3:] != 'YES':
-		time.sleep(0.2);
-		lines = read_temp_raw(device_file);
-		equals_pos = lines[1].find('t=');
-	temp_string = lines[1][equals_pos+2:];
-	temp_c = float(temp_string)/1000.0;
-	temp_f = temp_c * 9.0 / 5.0 + 32.0;
-	return (temp_c, temp_f);
+		time.sleep(0.2)
+		with open(file, 'r') as file:
+			lines = file.readlines()
+		equals_pos = lines[1].find('t=')
 
-def get_average_temp(device_file):
-	global AVERAGE_COUNT;
-	
+	temp_string = lines[1][equals_pos+2:]
+	temp_c = float(temp_string)/1000.0
+	temp_f = temp_c * 9.0/5.0 + 32.0
+	return (temp_c, temp_f)
+
+def get_average_temperature(file):	
 	total_f = 0;
-	for i in range(0,AVERAGE_COUNT):
-		temp_c, temp_f = read_temp(device_file);
-		total_f += temp_f;
-	return total_f/AVERAGE_COUNT;
+	for i in range(0, AVERAGE_COUNT):
+		temp_c, temp_f = read_temperature(file)
+		total_f += temp_f
+	return total_f/AVERAGE_COUNT
+# END TEMPERATURE #
 
-def post_current_temp(temp):
-	payload = {
-		'id':           TEMP_SENSOR_ID,
-		'key':          TEMP_SENSOR_KEY,
-		'temperature':  temp,
-		'time':         int(time.time())
-	};
-
-	try:
-		resp = requests.post(TEMP_UPDATE_URL, data=payload);
-		data = json.loads(resp.text);
-		if(data["success"] is True):
-			return True;
-	except:
-		return False;
-
-# FOR LIGHT DATA #
+# START LIGHT #
 def RCtime (RCpin):
         reading = 0
         GPIO.setup(RCpin, GPIO.OUT)
@@ -100,57 +74,48 @@ def RCtime (RCpin):
  
         GPIO.setup(RCpin, GPIO.IN)
         # This takes about 1 millisecond per loop cycle
-        while (GPIO.input(RCpin) == GPIO.LOW and reading < MAX_VALUE):
+        while (GPIO.input(RCpin) == GPIO.LOW and reading < LIGHT_MAX_VALUE):
                 reading += 1
-        return math.exp((-reading*10)/MAX_VALUE) * 100
+        return math.exp((-reading*10)/LIGHT_MAX_VALUE) * 100
+# END LIGHT #
 
-def post_current_light(light):
+def post_data(url, id, key, type, value):
 	payload = {
-		'id':           TEMP_SENSOR_ID,
-		'key':          TEMP_SENSOR_KEY,
-		'light':        light,
+		'id':           id,
+		'key':          key,
 		'time':         int(time.time())
-	};
+	}
+	payload[type] = value;
+	print('{:s}: ({:d}) {:1.3f}'.format(type, payload['time'], value))
 
 	try:
-		resp = requests.post(LIGHT_UPDATE_URL, data=payload);
-		data = json.loads(resp.text);
-		if(data["success"] is True):
-			return True;
+		resp = requests.post(url, data = payload)
+		data = json.loads(resp.text)
+		return data["success"] is True
 	except:
-		return False;
+		return False
 
-# END OF FOR LIGHT DATA #
-
-# First try to get the device file
-
-print('Finding sensor device file...\n');
-
-device_file = None;
-while (device_file is None):
-	print('Not found');
-	device_file = get_device_file();
-	time.sleep(0.5);
-
-print('\nDevice found!\n');
-
-# Now the actual sensor loop
+device_file = get_temperature_file()
 while (True):
-	avg_temp = -1;
 	try:
-		avg_temp = get_average_temp(device_file);
-		if(avg_temp > 0):
-			post_current_temp(avg_temp);
-			print('({:d}) {:1.3f} F'.format(int(time.time()), avg_temp));
+		avg_temp = -1
+		if(device_file is None):
+			print('Sensor not found')
+			device_file = get_temperature_file()
+		else:
+			avg_temp = get_average_temperature(device_file)
+			if(avg_temp > 0):
+				post_data(TEMP_UPDATE_URL, TEMP_SENSOR_ID, TEMP_SENSOR_KEY, 'temperature', avg_temp)
 	except KeyboardInterrupt:
-		break;
+		break
 	except Exception as e:
-		print('Error getting temp reading: '+str(e));
+		print('Error getting temp reading: ' + str(e))
 
-	light_value = RCtime(18);
-	post_current_light(light_value); # FOR LIGHT DATA #
-	# FOR LIGHT DATA #
-	print light_value     # Read RC timing using pin #18
-	# FOR LIGHT DATA #
-	
-	time.sleep(READ_DELAY);
+	try:
+		post_data(LIGHT_UPDATE_URL, LIGHT_SENSOR_ID, LIGHT_SENSOR_KEY, 'light', RCtime(LIGHT_PIN))
+	except KeyboardInterrupt:
+		break
+	except Exception as e:
+		print('Error getting light reading: ' + str(e))
+
+	time.sleep(READ_DELAY)
